b5507c14e593ef7c0d359179cf56224d0e38ea50
Ale - aledelsur <alealvarez_00@hotmail.com>
2012-09-27 21:17:58 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gydobjetos::Application.config.secret_token = '4a563633aee079e6f4a29e1d0b9f2faf695996de29aeb6bf2702ec923363fb01675b8cf916d780bbb4e652e7164242e28c7ecf969ef485687a9ef7cf53c789a0'
