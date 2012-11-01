d658359265c798793bad11b7074393367e026b16
Lucas Minissale <lucasminissale@gmail.com>
2012-11-01 19:28:11 -0300

    Finished categories filter. Changed asset pipeline configuration

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -6,31 +6,27 @@ class SiteController < ApplicationController
     unless session[:not_first_time]
       render "welcome_page", :layout => false
       session[:not_first_time] = true
-    end
-    if params[:event] == "quienes-somos"
-      @event = "quienes-somos"
-      @paginations = Photo.paginate :per_page => 12, :page => params[:page] #This array is for testing pagination. Will be deleted
-      @about_page = Page.find_by_key("quienes-somos")
-    elsif params[:event] == "contacto"
-      @event = "contacto"
-      @contact_page = Page.find_by_key("contacto")
-    elsif params[:event] == "hogar"
-      @event = "hogar"
-      @page = Page.find_by_key("hogar")
-      @products = @page.categories.collect{|c| c.products}.paginate(:per_page => 12, :page => params[:page]).flatten
-    elsif params[:event] == "bazar"
-      @event = "bazar"
-      @page = Page.find_by_key("bazar")
-      @products = @page.categories.collect{|c| c.products}.paginate(:per_page => 12, :page => params[:page]).flatten
-    elsif params[:event] == "petit"
-      @event = "petit"
-      @page = Page.find_by_key("petit")
-      @products = @page.categories.collect{|c| c.products}.paginate(:per_page => 12, :page => params[:page]).flatten
-    elsif params[:event] == "wedding-list"
-      @event = "wedding-list"
     else
-      @event = "home"
-      @home_photos = HomePhoto.all
+      @event = params[:event]
+      if params[:event] == "quienes-somos"
+        @paginations = Photo.paginate :per_page => 12, :page => params[:page] #This array is for testing pagination. Will be deleted
+        @about_page = Page.find_by_key("quienes-somos")
+      elsif params[:event] == "contacto"
+        @contact_page = Page.find_by_key("contacto")
+      elsif params[:event] == "hogar"
+        @page = Page.find_by_key("hogar")
+        #@products = @page.categories.collect{|c| c.products}.paginate(:per_page => 12, :page => params[:page]).flatten
+      elsif params[:event] == "bazar"
+        @page = Page.find_by_key("bazar")
+        #@products = @page.categories.collect{|c| c.products}.paginate(:per_page => 12, :page => params[:page]).flatten
+      elsif params[:event] == "petit"
+        @page = Page.find_by_key("petit")
+        #@products = @page.categories.collect{|c| c.products}.paginate(:per_page => 12, :page => params[:page]).flatten
+      else
+        @event = "home"
+        @home_photos = HomePhoto.all
+      end
+      @products = @page.get_products(params) if @page
     end  
   end
 
