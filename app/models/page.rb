13513466166d444e50b3436c09add84e93c26858
Ale - aledelsur <alealvarez_00@hotmail.com>
2012-11-02 15:47:50 -0300

    issue solved undefined method zero? for nil:NilClass

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,8 @@
 class Page < ActiveRecord::Base
   attr_accessible :title, :text, :key, :contact_address, :latitude, :longitude, :tel, :email, :meta_title, :meta_description, :meta_keywords
 
+  require 'will_paginate/array'
+
   has_many :home_photos
   has_many :categories, :dependent => :destroy
 
@@ -10,10 +12,10 @@ class Page < ActiveRecord::Base
 
   def get_products(params)
     unless params[:category_id]
-      categories.collect{|c| c.products}.paginate(:per_page => 12, :page => params[:page]).flatten
+      categories.collect{|c| c.products}.flatten.paginate(:per_page => 12, :page => params[:page])
     else
       category = Category.find(params[:category_id])
-      category.products.paginate(:per_page => 12, :page => params[:page]).flatten
+      category.products.flatten.paginate(:per_page => 12, :page => params[:page])
     end
   end
 end
